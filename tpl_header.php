39f44359437cb45c89b1c3e642c81df43d3bab73
Stefan Rothe <info@stefan-rothe.ch>
2014-10-02 10:04:50 +0200

    Improved layout: Menubar is not inside header anymore.

--- tpl_header.php
+++ tpl_header.php
@@ -1,13 +1,14 @@
 <header>
     <div id="logo"><?php tpl_link(wl(),'<img src="'.ml('logo.png').'" alt="'.$conf['title'].'" />','') ?></div>
     <h1><?php tpl_link(wl(), $conf['title'], 'accesskey="h" title="[H]"') ?></h1>
-    <?php if ($conf['tagline']) { ?><p class="claim"><?php echo $conf['tagline'] ?></p><?php } ?>
-    <?php if($showMenubar): ?>
-    <div class="clearer"></div>
-    <nav>
-        <div class="content">
-            <?php tpl_flush() ?>
-            <?php tpl_include_page(tpl_getConf('menubar'), 1, 1) ?>
-        </div>
-    </nav>
-    <?php endif; ?></header>
+    <?php if ($conf['tagline']): ?><p class="claim"><?php echo $conf['tagline'] ?></p><?php endif; ?>
+    <?php if($showMenubar): ?><div class="clearer"></div><?php endif; ?>
+</header>
+<?php if ($showMenubar): ?>
+<nav class="menubar">
+    <div class="content">
+        <?php tpl_flush() ?>
+        <?php tpl_include_page(tpl_getConf('menubar'), 1, 1) ?>
+    </div>
+</nav>
+<?php endif; ?>
